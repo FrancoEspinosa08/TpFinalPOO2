b484a6635d49e16c9290682dc49a885a79f78228
Medina, Ivan Angel <ivanangelmedina@gmail.com>
2024-11-11 18:34:53 -0300

    Se implemento todas las politicas de cancelacion del paquete  politicasDeCancelacion.

--- src/politicasDeCancelacion/CancelacionGratuita.java
+++ src/politicasDeCancelacion/CancelacionGratuita.java
@@ -1,5 +1,22 @@
 package politicasDeCancelacion;
 
-public class CancelacionGratuita {
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import observer.Inmueble;
+
+public class CancelacionGratuita extends PoliticaDeCancelacion{
+
+	@Override
+	public float aplicarPenalidad(Inmueble reservaACancelar, LocalDateTime diaDeLaCancelacion) {
+		
+		long diasDeAntelacion = ChronoUnit.DAYS.between(diaDeLaCancelacion, reservaACancelar.getFechaCheckIn());
+		
+		if (diasDeAntelacion >= 10) {
+			return 0; // Cancelación gratuita
+		} else {
+			return reservaACancelar.getPrecioPorDia() * 2; // Penalidad de dos días
+		}	
+	}
 
 }
