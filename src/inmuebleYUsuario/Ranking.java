ccf3b64c9618ec7d715b15a735ad1759770e0607
franco espinosa <francoespinosa.b@gmail.com>
2024-11-11 19:48:56 -0300

    ranking

--- src/inmuebleYUsuario/Ranking.java
+++ src/inmuebleYUsuario/Ranking.java
@@ -1,5 +1,25 @@
 package inmuebleYUsuario;
+import java.util.List;
+import administrador.Categoria;
 
-abstract class Ranking {
-
+public abstract class Ranking {
+	protected List<Categoria> categorias;
+	
+    // Devuelve el puntaje promedio de todas las categorías juntas (único puntaje)
+    public double promedioTotal(List<Categoria> categorias) {
+        // Suma todos los puntajes de las categorías
+    	double puntajeTotal = categorias.stream().map(cat -> cat.getPuntaje()).sum();
+    	int tamaño   = categorias.size();
+    	return puntajeTotal / tamaño;
+    }
+    // Constructor
+    public Ranking() {
+        // Inicializa la lista de categorías (puedes hacerlo de manera predeterminada o en una subclase)
+        this.categorias = null;  // O inicializar con una lista vacía si es necesario
+    }
+    
+    // Implementación común para la clase base
+    public List<Categoria> getCategorias() {
+        return categorias; // Devuelve la lista de las categorías de los inmuebles
+    }
 }
--- src/inmuebleYUsuario/RankingInmueble.java
+++ src/inmuebleYUsuario/RankingInmueble.java
@@ -1,5 +1,24 @@
 package inmuebleYUsuario;
+import java.util.List;
+import administrador.Categoria;
 
 public class RankingInmueble extends Ranking {
+
+    // Atributo
+    private List<Categoria> categoriasInmueble; // Lista de categorías de los inmuebles
+
+    // Métodos
+    public List<Categoria> getCategoriasInmueble() {
+        return categoriasInmueble; // Devuelve la lista de categorías de los inmuebles
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias; // Asigna la lista de categorías de los inmuebles
+    }
+
+    // Constructor
+    public RankingInmueble(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
 
 }
